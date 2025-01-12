2624cde136d4a27e6a81057d6041304c17758c44
Haining Pan <hnpan@terpmail.umd.edu>
2025-01-12 18:01:43 -0500

    add run_classA_2D_EE_ave.py

---
+++ run_classA_2D_EE.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+##SBATCH --exclude=cuda00[1-8],gpuc00[1-2],pascal0[01-10],gpu00[5-8],gpu0[10-14],gpu0[17-18],volta0[01-03]
+#SBATCH --exclude=cuda00[1-8],gpuc00[1-2],pascal0[01-10],volta0[01-03],gpu00[5-6]
+#SBATCH --time=14:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=8000
+#SBATCH --cpus-per-task=1
+#SBATCH --output=ARRARIDX.out
+#SBATCH --error=ARRARIDX.err
+
+ 
+cd $PWD
+
+module purge
+
+module load singularity
+
+PARAMS_FILE="$PWD/params.txt"
+# read -r  L mu nshell<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
+read -r  L mu nshell sigma<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
+
+# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_EE.py --L $L --nshell $nshell --mu $mu --es 50
+
+# normal script (legacy)
+# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_EE.py --L $L --nshell $nshell --mu $mu --es 50 --sigma $sigma --seed0 0
+
+# to be merge with es -50
+# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_EE.py --L $L --nshell $nshell --mu $mu --es 250 --sigma $sigma --seed0 50
+
+# normal script (legacy)
+srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_EE_ave.py --L $L --nshell $nshell --mu $mu --es 300 --sigma $sigma --seed0 0
+
+# To compute L log L
+# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_EE2.py --L $L --nshell $nshell --mu $mu --es 200 --sigma 0
+
+# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_EE_mp.py --L $L --nshell $nshell --mu $mu --es 50
---
+++ run_classA_2D_EE_ave.py
@@ -0,0 +1,87 @@
+from GTN2_torch import *
+import torch
+import argparse
+from tqdm import tqdm
+import time
+from utils_torch import *
+
+
+def measure_feedback_layer(gtn2,):
+    margin_x=0 
+    ilist = range(margin_x,gtn2.Lx-margin_x)
+    margin_y=0
+    jlist = range(margin_y,gtn2.Ly-margin_y)
+    ij_list = [(i,j) for i in (ilist) for j in (jlist)]
+    for i,j in (ij_list):
+        gtn2.measure_feedback(ij = [i,j])
+
+def randomize(gtn2,measure=True):
+    # for i in tqdm(range(2*gtn2.L+1,4*gtn2.L,2),desc='randomize'):
+    for i in range(2*gtn2.L+1,4*gtn2.L,2):
+        # print([i, (i+1)%(2*gtn2.L)+2*gtn2.L])
+        gtn2.randomize([i, (i+1)%(2*gtn2.L)+2*gtn2.L])
+    if measure:
+        # for i in (range(2*gtn2.L,4*gtn2.L,2),desc='measure'):
+        for i in (range(2*gtn2.L,4*gtn2.L,2)):
+            gtn2.measure_single_mode_Born([i,i+1],mode=[1])
+
+def randomize_inter(gtn2,scale=1):
+    # for i in tqdm(range(2*gtn2.L+1,4*gtn2.L,2),desc='randomize'):
+    for i in range(0,2*gtn2.L,2):
+        gtn2.randomize([i, (i+1)%(2*gtn2.L)],scale=scale)
+
+def run(inputs):
+    L, nshell,mu,sigma,seed=inputs
+    gtn2_torch=GTN2_torch(Lx=L,Ly=L,history=False,random_init=False,random_U1=True,bcx=1,bcy=1,seed=seed,orbit=2,nshell=nshell,layer=2,replica=1,complex128=True)
+
+    mu_list=[mu]
+    gtn2_torch.a_i={}
+    gtn2_torch.b_i={}
+    gtn2_torch.A_i={}
+    gtn2_torch.B_i={}
+    for mu in mu_list:
+        gtn2_torch.a_i[mu],gtn2_torch.b_i[mu] = amplitude(gtn2_torch.nshell,tau=[0,1],geometry='square',lower=True,mu=mu,C=1)
+        gtn2_torch.A_i[mu],gtn2_torch.B_i[mu] = amplitude(gtn2_torch.nshell,tau=[1,0],geometry='square',lower=False,mu=mu,C=1)
+    
+
+    A_idx_0,B_idx_0,C_idx_0 = gtn2_torch.generate_tripartite_circle()
+
+    for i in tqdm(range(gtn2_torch.Lx)):
+        measure_feedback_layer(gtn2_torch)
+        randomize(gtn2_torch,measure=True)
+        if sigma>0:
+            randomize_inter(gtn2_torch,scale=sigma)
+        
+    return gtn2_torch.C_m
+
+def dummy(inputs):
+    L, nshell,mu,sigma,seed=inputs
+    gtn2_torch=GTN2_torch(Lx=L,Ly=L,history=False,random_init=False,random_U1=True,bcx=1,bcy=1,seed=seed,orbit=2,nshell=nshell,layer=2,replica=1,complex128=True)
+    return gtn2_torch
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--L','-L',type=int)
+    parser.add_argument('--nshell','-nshell',type=int)
+    parser.add_argument('--mu','-mu',type=float)
+    parser.add_argument('--es','-es',type=int,default=10)
+    parser.add_argument('--seed0','-seed0',type=int,default=0)
+    parser.add_argument('--sigma','-sigma',type=float,default=0,help='local U(1) scale,  phase is [0,2pi*sigma]')
+
+    args=parser.parse_args()
+
+    st=time.time()
+    inputs=[(args.L, args.nshell, args.mu,args.sigma, seed+args.seed0) for seed in range(args.es)]
+    gtn2_dummy=dummy(inputs[0])
+    gtn2_dummy.C_m.zero_()
+    for inp in inputs:
+        gtn2_dummy.C_m+= run(inp)
+
+    gtn2_dummy.C_m/=args.es
+    nu=gtn2_dummy.chern_number_quick(selfaverage=True)
+    
+    fn=f'class_A_2D_L{args.L}_nshell{args.nshell}_mu{args.mu:.2f}_sigma{args.sigma:.3f}_es{args.es}_seed{args.seed0}_Chern_ave.pt'
+    torch.save({'Chern':nu,'args':args},fn)
+    
+    print('Time elapsed: {:.4f}'.format(time.time()-st))
+
+

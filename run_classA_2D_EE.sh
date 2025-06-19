f9eff572ec98f2a17cfe5858e88279cebff363f1
Haining Pan <hnpan@terpmail.umd.edu>
2025-06-19 10:40:34 -0400

    update

--- run_classA_2D_EE.sh
+++ run_classA_2D_EE.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=gpu
 ##SBATCH --exclude=cuda00[1-8],gpuc00[1-2],pascal0[01-10],gpu00[5-8],gpu0[10-14],gpu0[17-18],volta0[01-03]
 #SBATCH --exclude=cuda00[1-8],gpuc00[1-2],pascal0[01-10],volta0[01-03],gpu00[5-6]
-#SBATCH --time=12:20:00
+#SBATCH --time=36:20:00
 #SBATCH --ntasks=1
 #SBATCH --mem=8000
 #SBATCH --cpus-per-task=1
@@ -23,7 +23,7 @@ PARAMS_FILE="$PWD/params.txt"
 # read -r  L mu nshell sigma seed<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
 # read -r Lx Ly mu nshell sigma<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
 # read -r Lx Ly mu nshell sigma seed<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
-# read -r Lx Ly mu nshell sigma seed es<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
+read -r Lx Ly mu nshell sigma seed es<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
 
 # srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_EE.py --L $L --nshell $nshell --mu $mu --es 50
 
@@ -38,7 +38,7 @@ PARAMS_FILE="$PWD/params.txt"
 
 # normal script for all(most common), but with Lx,Ly assigned independently, tempararily use es=20 for "mu-tuned" and es=19 for "sigma-tuned"
 # srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_all.py --Lx $Lx --Ly $Ly --nshell $nshell --mu $mu --es 20 --sigma $sigma --seed0 0
-# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_all.py --Lx $Lx --Ly $Ly --nshell $nshell --mu $mu --es $es --sigma $sigma --seed0 $seed
+srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_all.py --Lx $Lx --Ly $Ly --nshell $nshell --mu $mu --es $es --sigma $sigma --seed0 $seed
 
 # compute the internal entropy SA
 # srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_EE_SA.py --Lx $Lx --Ly $Ly --nshell $nshell --mu $mu --es 20 --sigma $sigma --seed0 0 
@@ -59,8 +59,8 @@ PARAMS_FILE="$PWD/params.txt"
 # srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_DW.py --L $L --nshell $nshell --tf 2 --seed0 $seed  --es 10 
 
 # To compute dynamics (Chern and I2)
-read -r Lx Ly mu nshell tf sigma seed es<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
-srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_all_T.py --Lx $Lx --Ly $Ly --mu $mu --nshell $nshell --tf $tf --sigma $sigma --seed0 $seed --es $es
+# read -r Lx Ly mu nshell tf sigma seed es<<< $(sed -n "ARRARIDXp" $PARAMS_FILE)
+# srun singularity exec --nv /scratch/hp636/pytorch.sif python run_classA_2D_all_T.py --Lx $Lx --Ly $Ly --mu $mu --nshell $nshell --tf $tf --sigma $sigma --seed0 $seed --es $es
 
 # python run_classA_2D_all.py --Lx 8 --Ly 8 --nshell 2 --mu 1. --es 2 --sigma 0 --seed0 0 
 # python run_classA_2D_OP_T.py --L 5 --nshell 2 --mu 1 --es 10 --sigma 0.1 --seed0 0 --tf 2
